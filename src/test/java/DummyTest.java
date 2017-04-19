c695f3b8d9b2fb3629d7f734a85f0c03a75fa889
Marius Reusch <marius.reusch@googlemail.com>
2017-04-19 21:07:06 +0200

    Initial commit

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() throws Exception {
+
+        assertTrue(true);
+    }
+}
